f12060ae3ae82c892d45f7b5d58e44ed39f9f6c3
James McAdam <j.g.mcadam@gmail.com>
2018-08-11 01:57:35 +0100

    Created a Scene with a bunch of test satalites that use 2d physics to orbit. They collide with the planet and explode. The orbit applied is a bit odd at the moment as this is for testing.

---
+++ LD42-RunningOutOfSpace/Assets/_Scripts/test_Planet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_Planet : MonoBehaviour {
+
+	public float gravity = 9.8f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ LD42-RunningOutOfSpace/Assets/_Scripts/test_Satalite.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_Satalite : MonoBehaviour {
+
+	public float initialForce = 1000.0f;
+	public float mass = 1.0f;
+	test_Planet planet;
+	float radius;
+	Rigidbody2D rb;
+	float dragFactor = 0.01f;
+	float timeToCrash = 0.5f;
+	float timer = 0.0f;
+	public GameObject explosion;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+		// Get a reference to the planet 
+		planet = FindObjectOfType<test_Planet> ().GetComponent<test_Planet>(); 
+
+		// Find out the starting radius
+		radius = (planet.transform.position - transform.position).magnitude;
+
+		// Get a reference to a rigid body 
+		rb = GetComponent<Rigidbody2D>();
+
+		// Find the angle of the statlite to the planet
+		float angle = Mathf.Atan2(transform.position.y, transform.position.x);
+
+		// Find the direction of the tangential force based on the starting position ( Might not be required if we use rockets to accelerate the satalites up there)
+		Vector2 forceDir = new Vector2(-transform.position.y/Mathf.Tan(90*Mathf.Deg2Rad - angle), radius*Mathf.Sin(angle));
+
+		// Add the initial force to make the satalite orbit
+		rb.AddForce(forceDir.normalized * initialForce);
+
+	}
+	
+	// Update the physics
+	void FixedUpdate()
+	{
+
+		// calculate the distance
+		float distance = (planet.transform.position - transform.position).sqrMagnitude;
+
+		// Apply a current force towards the planet f = mv/r^2
+		rb.AddForce ((planet.transform.position - transform.position).normalized * planet.gravity * mass / distance);
+
+		// Apply the current drag factor
+		rb.velocity -= rb.velocity*dragFactor*Time.deltaTime;
+
+	}
+
+	// Need to test for collision with planet and other statalites
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+		// Check if it's the planet and if so destroy this Satelite (Need to make this onto a tag)
+		if (other.gameObject.name == "TestPlanet")
+		{
+			// increase the drag
+			mass *= 3.0f;
+			// Start the crash procedure
+			StartCoroutine (Crash());
+		}
+
+		// To be completed
+		if (other.gameObject.name == "testAtmosphere")
+		{
+
+		}
+
+	}
+
+	// If it's too close bring it in and crash
+	IEnumerator Crash()
+	{
+
+		while (timer < timeToCrash)
+		{
+			timer += Time.deltaTime;
+			transform.localScale *= ((timeToCrash - timer) / timeToCrash);
+			yield return null;
+		}
+			
+		// Spawn a wee explosion (which will destoy itself after some time)
+		GameObject explode = Instantiate (explosion).gameObject;
+
+		// remove the parent
+		explode.transform.parent = null;
+
+		// Set the position
+		explode.transform.position = gameObject.transform.position;
+
+		// Reset the size
+		explode.transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+				
+		// Destroy this 
+		Destroy (this.gameObject);
+
+	}
+		
+}
