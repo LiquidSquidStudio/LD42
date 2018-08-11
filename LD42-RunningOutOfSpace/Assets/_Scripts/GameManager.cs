2e2085dd1929b33380c7e84f1132a9a6f568be75
James McAdam <j.g.mcadam@gmail.com>
2018-08-11 14:40:45 +0100

    Started the game manager to run the main game loop

--- LD42-RunningOutOfSpace/Assets/_Scripts/GameManager.cs
+++ LD42-RunningOutOfSpace/Assets/_Scripts/GameManager.cs
@@ -4,13 +4,99 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	// Game mode tracking
+	enum Mode { Start, Game, End };
+	Mode current = Mode.Start;
+
+	// Gonna need reference to the objects to spawn
+	test_Rocket player = null;
+	test_Satalite player_Controlled = null;
+
+	// Keep a track of all the active satillites to count for points
+	public List<test_Satalite> satalites = new List<test_Satalite>();
+
+	// Game Score
+	float gameScore = 0.0f;
+	int totalSatalites = 0;
+
+	// Game State tracking
+
 	// Use this for initialization
 	void Start () {
+
+		// The game manager will persist and be reset
+		DontDestroyOnLoad(this.gameObject);
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+
+		switch (current)
+		{
+		case Mode.Start:
+
+			RestGame ();
+			StartGame ();
+			current = Mode.Game;
+
+			break;
+
+
+		case Mode.Game:
+
+			// Add score and spawn stuff
+		
+			break;
+
+
+		case Mode.End:
+
+			// Display Score 
+
+			break;
+		}
 		
 	}
+
+
+	void StartGame()
+	{
+
+		// Spawn in the player and setup an other required conditions
+
+	}
+
+	void RestGame()
+	{
+		
+		// Reset the score
+		gameScore = 0.0f;
+		totalSatalites = 0;
+
+		// If there is player objects ect destroy them and reset
+		if (player != null)
+		{
+			Destroy (player);
+			player = null;
+		}
+
+		if (player_Controlled != null)
+		{
+			Destroy (player_Controlled);
+			player_Controlled = null;
+		}
+
+		// Destroy the remaining satillites 
+		foreach (var sat in satalites)
+		{
+			Destroy (sat);
+		}
+
+		// Clear the list
+		satalites.Clear();
+
+	}
+		
 }
