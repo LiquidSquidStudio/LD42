d45064f7baae62fa729d6769e48127a27c12fcc4
DESKTOP-4P9ASIC\Ross <rossedrummond@gmail.com>
2018-08-13 21:30:23 +0100

    testing scene

---
+++ LD42-RunningOutOfSpace/Assets/Gravity.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+    // Attach this component to anything that needs to get effected by gravity.
+    // Need to make a corresponding script for the gravity "source"
+
+    GravitySource gravitySource;
+    public float mass;
+    public bool effectedByGravity = true;
+
+    Rigidbody2D rb;
+    Vector2 forceDueToGravity;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        gravitySource = FindObjectOfType<GravitySource>();
+    }
+
+    // Force due to gravity is calculated using the formula: F = Gm1m2/r2
+    // Returned as a 2D vector for direction as well
+    Vector2 GravityForce()
+    {
+        Vector2 distanceVector = gravitySource.transform.position - this.transform.position;
+        float distance = distanceVector.magnitude;
+
+        float force = mass * gravitySource.massAndG / (distance*distance);
+
+        Vector2 direction = distanceVector.normalized;
+
+        forceDueToGravity = direction * force;
+
+        return forceDueToGravity;
+    }
+
+    // Apply force due to gravity only if the bool is checked
+    private void FixedUpdate()
+    {
+        if (effectedByGravity)
+        {
+            rb.AddForce(GravityForce());
+        }
+    }
+}
---
+++ LD42-RunningOutOfSpace/Assets/TestLaunch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLaunch : MonoBehaviour {
+
+    GameObject rocketPrefab;
+    GameObject currentRocket;
+
+    public int launchAngle;
+    public float launchForce;
+    public float burnTime;
+    Vector2 mouseDir;
+
+    bool needANewRocket = false;
+
+	void Update () {
+
+        // Press enter to spawn a new rocket if there isn't one already
+        if (needANewRocket && Input.GetButtonDown("Enter"))
+        {
+            SpawnRocket();
+        }
+
+        // Press spacebar to launch rocket if there is one
+        if (!needANewRocket && Input.GetButtonDown("Jump"))
+        {
+            LaunchRocket();
+        }
+	}
+
+    void SpawnRocket()
+    {
+        currentRocket = Instantiate(rocketPrefab, this.transform);
+    }
+
+    void LaunchRocket()
+    {
+
+    }
+
+}
